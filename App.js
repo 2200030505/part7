b60855611e5637215a02752c576f8430a21a724c
2200030505 <147685771+2200030505@users.noreply.github.com>
2023-10-12 12:05:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+return (
+<div>
+
+<h1> State Management </h1>
+<Counter />
+
+</div>
+);
+
+};
+export default App;
