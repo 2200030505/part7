79bac79f0d5b252a71ef82e08908bbd982cf91d1
2200030505 <147685771+2200030505@users.noreply.github.com>
2023-10-12 13:11:57 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Counter from './Counter';
 
-const App = () => {
-return (
-<div>
+import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import {  Route, Routes } from 'react-router-dom';
 
-<h1> State Management </h1>
-<Counter />
 
-</div>
-);
+function App() {
+  return (
+    
+    
+    <Routes>
+      <Route path="/" element= {<Home/>}></Route>
+      <Route path="About" element={<About/>}></Route>
+    </Routes>
+      
+  );
+}
 
-};
 export default App;
